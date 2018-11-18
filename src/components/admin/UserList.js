a53757f8ac432717a5daf62c2b3d40dbe06d15f6
Krystof Tulinger <k.tulinger@seznam.cz>
2018-11-18 14:42:57 +0100

    add online status

--- src/components/admin/UserList.js
+++ src/components/admin/UserList.js
@@ -17,6 +17,27 @@ class User extends React.Component {
         this.props.logAs(this.props.id);
     }
 
+    dateDiff(d1, d2) {
+        if (d1.getFullYear() !== d2.getFullYear()) {
+            return `${d1.getFullYear() - d2.getFullYear()} let`
+        }
+        if (d1.getMonth() !== d2.getMonth()) {
+            return `${d1.getMonth() - d2.getMonth()} měsíců`
+        }
+        if (d1.getDate() !== d2.getDate()) {
+            return `${d1.getDate() - d2.getDate()} dnů`
+        }
+        if (d1.getHours() !== d2.getHours()) {
+            return `${d1.getHours() - d2.getHours()} hodin`
+        }
+        if (d1.getMinutes() !== d2.getMinutes()) {
+            return `${d1.getMinutes() - d2.getMinutes()} minut`
+        }
+        if (d1.getSeconds() !== d2.getSeconds()) {
+            return `${d1.getSeconds() - d2.getSeconds()} vteřin`
+        }
+    }
+
     render() {
         const {currentUser, roles, id, name, last_click_at, surname, email} = this.props;
         return (
@@ -42,15 +63,15 @@ class User extends React.Component {
                     </Link>
                 </td>
                 <td>
-                {currentUser.id !== id && (
+                    {currentUser.id !== id && (
                         <button
                             type="button"
                             className={classnames('btn', 'badge', 'btn-success')}
                             onClick={this.onLogAs}
                         >
-                                Přihlasit se
+                            Přihlasit se
                         </button>
-                )}
+                    )}
                 </td>
                 <td>
                     {last_click_at && last_click_at > new Date(new Date().getTime() - 30 * 60 * 1000) ? (
@@ -64,6 +85,16 @@ class User extends React.Component {
                             title={'Poslední klik ' + last_click_at.toLocaleString()}
                         >
                             online
+                        </span>
+                    ) : null}
+                    {last_click_at && last_click_at <= new Date(new Date().getTime() - 30 * 60 * 1000) ? (
+                        <span className="badge badge-dark" title={'Poslední klik ' + last_click_at.toLocaleString()}>
+                            {this.dateDiff(new Date(), last_click_at)}
+                        </span>
+                    ) : null}
+                    {!last_click_at ? (
+                        <span className="badge badge-dark" title={'Poslední klik nikdy'}>
+                            nikdy
                         </span>
                     ) : null}
                 </td>
@@ -112,20 +143,20 @@ class UserList extends Component {
                 Z přehledu můžete pokračovat na přehled uživatele a nebo se můžete přihlásit pod uživatelovou identitou.
                 <table className="table">
                     <thead>
-                        <tr>
-                            <th>Číslo</th>
-                            <th>Jméno</th>
-                            <th>Příjmení</th>
-                            <th>Role</th>
-                            <th>Email</th>
-                            <th>Odkaz</th>
-                            <th>Přihlásit se</th>
-                            <th>Online</th>
-                        </tr>
+                    <tr>
+                        <th>Číslo</th>
+                        <th>Jméno</th>
+                        <th>Příjmení</th>
+                        <th>Role</th>
+                        <th>Email</th>
+                        <th>Odkaz</th>
+                        <th>Přihlásit se</th>
+                        <th>Online</th>
+                    </tr>
                     </thead>
                     <tbody>
                     {users.map(user => (
-                        <User key={user.id} {...user} match={match} />
+                        <User key={user.id} {...user} match={match}/>
                     ))}
                     </tbody>
                     <tfoot>
